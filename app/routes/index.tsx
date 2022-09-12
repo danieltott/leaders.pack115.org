ff200b5c605852039b6b78c0afacbe28ec46da6a
Dan Ott <dan@dtott.com>
2022-09-12 16:46:34 -0400

    initial commit from stack

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+import { Link } from "@remix-run/react";
+
+export default function Index() {
+  return (
+    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
+      <h1>Pack 115 Leaders Portal</h1>
+    </main>
+  );
+}
