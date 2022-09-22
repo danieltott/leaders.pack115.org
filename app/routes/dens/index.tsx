b29b10ac25bd96931cd79e4d440f4c3e77462dca
Dan Ott <dan@dtott.com>
2022-09-22 14:26:05 -0400

    Add copy den leader emails

--- app/routes/dens/index.tsx
+++ app/routes/dens/index.tsx
@@ -1,8 +1,43 @@
 import { Link, useLoaderData } from "@remix-run/react";
-import { json, LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import type { LoaderArgs } from "@remix-run/node";
 import Layout from "~/components/Layout";
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
 import { fetchAllData } from "~/data";
+import type { Data, Ids } from "../../../types";
+import { useMemo, useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { CheckIcon } from "@heroicons/react/20/solid";
+
+export function CopyEmailsFromLeaders({ ids, data }: { ids: Ids; data: Data }) {
+  const [copied, setCopied] = useState(false);
+
+  const allEmails = useMemo(() => {
+    const list = ids.dens.reduce<string[]>((acc, denId) => {
+      const den = data.dens[denId];
+      if (den["Den Leader Emails"] && den["Den Leader Emails"].length > 0) {
+        return [...acc, ...den["Den Leader Emails"]];
+      }
+
+      return acc;
+    }, []);
+    return list.join(", ");
+  }, [data.dens, ids.dens]);
+
+  return (
+    <CopyToClipboard text={allEmails} onCopy={() => setCopied(true)}>
+      <button
+        type="button"
+        className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      >
+        {copied && (
+          <CheckIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+        )}
+        Copy Den Leader Emails
+      </button>
+    </CopyToClipboard>
+  );
+}
 
 export async function loader(args: LoaderArgs) {
   const allData = await fetchAllData();
@@ -16,7 +51,10 @@ export default function Index() {
   return (
     <Layout title="Dens">
       <div className="overflow-hidden bg-white shadow sm:rounded-md">
-        <ul role="list" className="divide-y divide-gray-200">
+        <div className="border-b border-gray-200 px-4 py-4 text-right sm:px-6">
+          <CopyEmailsFromLeaders data={data} ids={ids} />
+        </div>
+        <ul className="divide-y divide-gray-200">
           {ids.dens.map((denId) => {
             const den = data.dens[denId];
             return (
